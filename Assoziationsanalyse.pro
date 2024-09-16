40670cdb135ed1a83ab96230a0bf783115faecfb
Patryk Mleczko <mleczko.patryk.roman@gmail.com>
2024-09-16 22:48:44 +0200

    Zu einem Microservice umgebaut

--- Assoziationsanalyse.pro
+++ Assoziationsanalyse.pro
@@ -1,4 +1,4 @@
-QT = core
+QT += core httpserver
 
 CONFIG += c++17 cmdline
 
--- main.cpp
+++ main.cpp
@@ -1,29 +1,27 @@
 #include <QCoreApplication>
+#include <QPair>
 #include <QString>
 #include <QMap>
 #include <QSet>
 
-static QSet<QString> items;
+#include <QCommandLineParser>
+#include <QCommandLineOption>
+#include <QScopedPointer>
+#include <QSettings>
+#include <QUuid>
+#include <QDebug>
+#include <QDir>
 
-static QVector<QStringList> transaktionen
-{
-    {"sunny", "hot", "high", "false", "no"},
-    {"sunny", "hot", "high", "true", "no"},
-    {"overcast", "hot", "high", "false", "yes"},
-    {"rainy", "mild", "high", "false", "yes"},
-    {"rainy", "cool", "normal", "false", "yes"},
-    {"rainy", "cool", "high", "false", "no"},
-    {"overcast", "cool", "normal", "true", "yes"},
-    {"sunny", "mild", "high", "true", "no"},
-    {"sunny", "cool", "normal", "false", "yes"},
-    {"rainy", "mild", "high", "true", "yes"},
-    {"sunny", "mild", "normal", "true", "yes"},
-    {"overcast", "mild", "normal", "true", "yes"},
-    {"overcast", "hot", "normal", "false", "yes"},
-    {"rainy", "mild", "high", "true", "no"},
-};
+#include <QtConcurrent/QtConcurrent>
+#include <QFutureInterface>
+#include <QFuture>
 
-static QMap<QString, qreal> itemToSupport;
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+#include <QtHttpServer>
+#include <QHostAddress>
 
 struct Confidence
 {
@@ -37,66 +35,241 @@ struct Lift
     double lift;
 };
 
-static QMap<QString, qreal> itempairNameToSupport;
-static QMap<QString, QVector<Confidence> > nameToConfidences;
-static QMap<QString, QVector<Lift> > nameToLifts;
-
 int main(int argc, char *argv[])
 {
-    QCoreApplication a {argc, argv};
+    QCoreApplication app {argc, argv};
 
-    for (const QStringList &transaktion : transaktionen)
-        for (const QString &entry : transaktion)
-            items << entry;
+    const quint16 PORT {50000};
 
-    for (const QString &item : items)
+    const QScopedPointer<QHttpServer> httpServer {new QHttpServer {&app}};
+
+    httpServer->route("/associationanalysis", QHttpServerRequest::Method::Get     |
+                                              QHttpServerRequest::Method::Put     |
+                                              QHttpServerRequest::Method::Head    |
+                                              QHttpServerRequest::Method::Trace   |
+                                              QHttpServerRequest::Method::Patch   |
+                                              QHttpServerRequest::Method::Delete  |
+                                              QHttpServerRequest::Method::Options |
+                                              QHttpServerRequest::Method::Connect |
+                                              QHttpServerRequest::Method::Unknown,
+    [](const QHttpServerRequest &request) -> QFuture<QHttpServerResponse>
     {
-        double itemCount {0.0};
+        Q_UNUSED(request)
 
-        for (const QStringList &transaktion : transaktionen)
-            itemCount += transaktion.count(item);
-
-        itemToSupport.insert(item, itemCount / transaktionen.size());
-    }
-
-    for (const QString &firstItem : items)
-    {
-        QVector<Confidence> confidences;
-        QVector<Lift> lifts;
-
-        for (const QString &secondItem : items)
+        return QtConcurrent::run([]()
         {
-            if (secondItem == firstItem)
+            return QHttpServerResponse
             {
-                confidences << Confidence {secondItem, 0.0};
-                continue;
+                QJsonObject
+                {
+                    {"Message", "The used HTTP-Method is not implemented."}
+                }
+            };
+        });
+    });
+
+    httpServer->route("/associationanalysis", QHttpServerRequest::Method::Post,
+    [](const QHttpServerRequest &request) -> QFuture<QHttpServerResponse>
+    {
+        if (request.body().isEmpty())
+            return QtConcurrent::run([]()
+            {
+                return QHttpServerResponse
+                {
+                    QJsonObject
+                    {
+                        {"Message", "HTTP-Request body is empty."}
+                    }
+                };
+            });
+
+        const QJsonDocument jsonDocument {QJsonDocument::fromJson(request.body())};
+
+        if (jsonDocument.isNull())
+            return QtConcurrent::run([]()
+            {
+                return QHttpServerResponse
+                {
+                    QJsonObject
+                    {
+                        {"Message", "Invalid data sent. Please send a valid JSON-Object."}
+                    }
+                };
+            });
+
+        const QJsonObject jsonObject {jsonDocument.object()};
+
+        if (jsonObject.isEmpty())
+            return QtConcurrent::run([]()
+            {
+                return QHttpServerResponse
+                {
+                    QJsonObject
+                    {
+                        {"Message", "Invalid data sent. Please send a valid JSON-Object."}
+                    }
+                };
+            });
+
+        const QVector<QPair<QString, QStringList> > transaktionen = [](const QJsonObject &jsonObject) -> QVector<QPair<QString, QStringList> >
+        {
+            QVector<QPair<QString, QStringList> > transaktionen;
+
+            for (const QString &jsonKey : jsonObject.keys())
+                transaktionen << QPair<QString, QStringList>{jsonKey, jsonObject.value(jsonKey).toVariant().toStringList()};
+
+            return transaktionen;
+
+        }(jsonObject);
+
+        const QSet<QString> items = [](const QVector<QPair<QString, QStringList> > &transaktionen) -> QSet<QString>
+        {
+            QSet<QString> items;
+
+            for (const QPair<QString, QStringList> &transaktion : transaktionen)
+                for (const QString &transaktionsEntry : transaktion.second)
+                    items << transaktionsEntry;
+
+            return items;
+
+        }(transaktionen);
+
+        const QMap<QString, qreal> itemToSupport = [](const QSet<QString> &items, const QVector<QPair<QString, QStringList> > &transaktionen) -> QMap<QString, qreal>
+        {
+            QMap<QString, qreal> itemToSupport;
+
+            for (const QString &item : std::as_const(items))
+            {
+                double itemCount {0.0};
+
+                for (const QPair<QString, QStringList> &transaktion : transaktionen)
+                    itemCount += transaktion.second.count(item);
+
+                itemToSupport.insert(item, itemCount / transaktionen.size());
             }
 
-            double itemCountFirstAndSecondItem {0.0};
+            return itemToSupport;
 
-            for (const QStringList &transaktion : transaktionen)
+        }(items, transaktionen);
+
+        QMap<QString, qreal> itempairNameToSupport;
+        QMap<QString, QVector<Confidence> > nameToConfidences;
+        QMap<QString, QVector<Lift> > nameToLifts;
+
+        for (const QString &firstItem : items)
+        {
+            QVector<Confidence> confidences;
+            QVector<Lift> lifts;
+
+            for (const QString &secondItem : items)
             {
-                if (transaktion.contains(firstItem) && transaktion.contains(secondItem))
-                    ++itemCountFirstAndSecondItem;
+                if (secondItem == firstItem)
+                {
+                    continue;
+                    confidences << Confidence {secondItem, 0.0};
+                }
+
+                double itemCountFirstAndSecondItem {0.0};
+
+                for (const QPair<QString, QStringList> &transaktion : transaktionen)
+                {
+                    if (transaktion.second.contains(firstItem) && transaktion.second.contains(secondItem))
+                        ++itemCountFirstAndSecondItem;
+                }
+
+                const qreal supportAB  {(itemCountFirstAndSecondItem / transaktionen.size())};
+                const qreal supportA   {itemToSupport[firstItem]};
+                const qreal supportB   {itemToSupport[secondItem]};
+
+                const Confidence confidence {secondItem, supportAB / supportA};
+                confidences << confidence;
+
+                const Lift lift {secondItem, supportAB / (supportA * supportB)};
+                lifts << lift;
+
+                //if (!itempairNameToSupport.contains(firstItem + ":" + secondItem) && !itempairNameToSupport.contains(secondItem + ":" + firstItem))
+                    itempairNameToSupport.insert(firstItem + ":" + secondItem, supportAB);
             }
 
-            const qreal supportAB  {(itemCountFirstAndSecondItem / transaktionen.size())};
-            const qreal supportA   {itemToSupport[firstItem]};
-            const qreal supportB   {itemToSupport[secondItem]};
-
-            const Confidence confidence {secondItem, supportAB / supportA};
-            confidences << confidence;
-
-            const Lift lift {secondItem, supportAB / (supportA * supportB)};
-            lifts << lift;
-
-            if (!itempairNameToSupport.contains(firstItem + ":" + secondItem) && !itempairNameToSupport.contains(secondItem + ":" + firstItem))
-                itempairNameToSupport.insert(firstItem + ":" + secondItem, supportAB);
+            nameToConfidences.insert(firstItem, confidences);
+            nameToLifts.insert(firstItem, lifts);
         }
 
-        nameToConfidences.insert(firstItem, confidences);
-        nameToLifts.insert(firstItem, lifts);
-    }
+        const QJsonObject lifts = [](const QMap<QString, QVector<Lift> > &nameToLifts) -> QJsonObject
+        {
+            QJsonObject lifts;
 
-    return a.exec();
+            for (const QString &name : nameToLifts.keys())
+            {
+                QJsonArray jsonArray;
+
+                for (const Lift &lift : nameToLifts.value(name))
+                    jsonArray.append(QJsonObject{{lift.name, lift.lift}});
+
+                lifts.insert(name, jsonArray);
+            }
+
+            return lifts;
+
+        }(nameToLifts);
+
+        const QJsonObject confidences = [](const QMap<QString, QVector<Confidence> > &nameToConfidences) -> QJsonObject
+        {
+            QJsonObject confidences;
+
+            for (const QString &name : nameToConfidences.keys())
+            {
+                QJsonArray jsonArray;
+
+                for (const Confidence &confidence : nameToConfidences.value(name))
+                    jsonArray.append(QJsonObject{{confidence.name, confidence.confidence}});
+
+                confidences.insert(name, jsonArray);
+            }
+
+            return confidences;
+
+        }(nameToConfidences);
+
+        const QJsonObject supports = [](const QMap<QString, qreal> &itempairNameToSupport, const QSet<QString> &items) -> QJsonObject
+        {
+            QJsonObject supports;
+
+            for (const QString &itempairName : itempairNameToSupport.keys())
+            {
+                const QString firstItemName {itempairName.split(":").first()};
+
+                QJsonArray jsonArray;
+
+                for (const QString &item : items)
+                {
+                    if (itempairNameToSupport.keys().contains(firstItemName + ":" + item))
+                        jsonArray.append(QJsonObject{{item, itempairNameToSupport.value(firstItemName + ":" + item)}});
+                }
+
+                supports.insert(firstItemName, jsonArray);
+            }
+
+            return supports;
+
+        }(itempairNameToSupport, items);
+
+        return QtConcurrent::run([=]()
+        {
+            return QHttpServerResponse
+            {
+                QJsonObject
+                {
+                    {"Confidences", confidences},
+                    {"Lifts",       lifts},
+                    {"Supports",    supports}
+                }
+            };
+        });
+    });
+
+    if (httpServer->listen(QHostAddress::LocalHost, static_cast<quint16>(PORT)) == 0)
+        return -1;
+
+    return app.exec();
 }
